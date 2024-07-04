6cc99f506783a71a12b7508ccb8b2a21124c19f9
GezaKassay <kassay_geza@yahoo.com>
2024-07-04 15:34:51 +0300

    Optimized jscript

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-let clickCounter = 0
+let clickCounter = 0;
+let color = ["yellow", "green", "red"];
 
-function changeColor(_this) {
-    _this.style.backgroundColor = "yellow"
-    ++clickCounter
-    if(clickCounter == 2) {
-        _this.style.backgroundColor = "green"
-    }
-    if(clickCounter == 3) {
-        _this.style.backgroundColor = "red"
-        clickCounter = 0
+function changeColor(_button) {
+    _button.style.backgroundColor = color[clickCounter];
+    ++clickCounter;    
+    if (clickCounter == 3) {       
+        clickCounter = 0;
     }    
 }
